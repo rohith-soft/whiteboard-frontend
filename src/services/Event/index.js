34894ff9b6e76a3a629a53a8b6c523f968401a79
rohithZE <rohith.chilupuri@zemosolabs.com>
2021-02-11 19:50:40 +0530

    Fix async await in service layer

--- src/services/Event/index.js
+++ src/services/Event/index.js
@@ -3,27 +3,28 @@ import axios from "axios";
 import * as moment from "moment";
 
 export const fetchAllEvents = async (queryParams) => {
-  console.log("HP rows1.5 " );
+  console.log("HP rows1.5 ");
 
   const url = urls.getAllEvents + queryParams;
-  await axios
-    .get(url)
-    .then((resp) => {
-      if (resp.status == 200) {
-        if (resp.data.length > 0) {
-          resp.data.map((whiteBoardEvent) => {
-            var taskId = whiteBoardEvent["task_id"];
-            whiteBoardEvent["task_id"] = window.localStorage.getItem(taskId);
-            var status = whiteBoardEvent["status"];
-            whiteBoardEvent["status"] = statusContants[status];
-            var dateTime = whiteBoardEvent["scheduled"];
-            whiteBoardEvent["scheduled"] = `${moment(dateTime).format("L")} ${moment(dateTime).format("LT")}`;
-          });
-        }
-        return resp.data;
+  try {
+    const resp = await axios.get(url);
+
+    if (resp.status === 200) {
+      if (resp.data.length > 0) {
+        resp.data.map((whiteBoardEvent) => {
+          var taskId = whiteBoardEvent["task_id"];
+          whiteBoardEvent["task_id"] = window.localStorage.getItem(taskId);
+          var status = whiteBoardEvent["status"];
+          whiteBoardEvent["status"] = statusContants[status];
+          var dateTime = whiteBoardEvent["scheduled"];
+          whiteBoardEvent["scheduled"] = `${moment(dateTime).format(
+            "L"
+          )} ${moment(dateTime).format("LT")}`;
+        });
       }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+      return resp.data;
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
